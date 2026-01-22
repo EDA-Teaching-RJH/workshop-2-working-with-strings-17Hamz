645635087c369b5a962f618a3650e00f6e45c68b
17Hamz <iah6@kent.ac.uk>
2026-01-22 12:12:46 +0000

    grade calculator

--- If-Elif-Else Statement.py
+++ If-Elif-Else Statement.py
@@ -1,2 +1,18 @@
 def main():
-    
+ score = int(input("Score: "))
+ check_grade(score)
+
+
+def check_grade(score):
+    if score  >= 90 and score <= 100:
+        print ("A")
+    elif score >=80 and score <= 89:
+        print ("B")
+    elif score >=70 and score < 79:
+        print ("C")
+    elif score >=60 and score < 69:
+        print ("D")
+    else:
+        print ("F")
+
+main()
